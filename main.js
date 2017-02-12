5e55d0bcff71f26d4832054fcd49dc4be0c7f7a2
Mohammed <admin@joomlar.net>
2017-02-12 17:50:25 +0000

    o

--- main.js
+++ main.js
@@ -1,8 +1,66 @@
+Vue.component('task-list', {
+  template: `
+  <div>
+  <ul>
+  <task v-for="task in tasks">{{task.dec}}</task>
+  </ul>
+  </div>
+  `,
 
-Vue.component('task', {
-  template: '<li>My Task</li>'
+  data(){
+    return {
+      tasks: [
+      {dec: 'task1 description', complete: true},
+      {dec: 'task2 description', complete: false},
+      {dec: 'task3 description', complete: false},
+      {dec: 'task4 description', complete: false},
+      {dec: 'task5 description', complete: true}
+      ]
+    };
+  }
 });
 
+
+
+Vue.component('modal', {
+
+  props: ['buttontext', 'modaltext', 'modalid'],
+
+
+  template: `
+<div>
+<button type="button" class="btn btn-primary" data-toggle="modal" :data-target="'#'+modalid">
+{{buttontext}}
+</button>
+
+<div class="modal fade bs-example-modal-lg" tabindex="-1" :id="modalid" role="dialog" aria-labelledby="myLargeModalLabel">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      {{modaltext}}
+    </div>
+  </div>
+</div>
+</div>
+`
+
+});
+
+
+
+Vue.component('task', {
+  template: '<li><slot></slot></li>'
+});
+
+Vue.component('jumbo', {
+  props: ['title', 'message'],
+  template: `
+  <div class="jumbotron">
+  <h1>{{title}}</h1>
+  <p>{{message}}</p>
+  <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+</div>
+`,
+})
 
 
 
@@ -13,7 +71,8 @@ new Vue({
     names: ['Hicham', 'Ghizlane', ' Fatima Ezzahra', 'Zainab', 'AbdAllah'],
     status: false,
     danger: false,
-    ttt: 'I am a button'
+    ttt: 'I am a button',
+    seeme: true,
   },
 
   methods: {
